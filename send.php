2b23cff2cc5b09fcd8139320f6a82927353f3c92
NurlanKosta <djunus-93@mail.ru>
2020-10-09 20:38:19 +0600

    fixed: finish

--- send.php
+++ send.php
@@ -56,3 +56,56 @@ else {$result = "error";}
 
 // Отображение результата
 header('location: thankyou.html');
+
+
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$mail = $_POST['mail'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое почта</h2>
+<b>Почта:</b> $mail<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'besttour101@gmail.com'; // Логин на почте
+    $mail->Password   = 'BestTour2020'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('besttour101@gmail.com', 'Нурлан Джунусов'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('djunus-93@mail.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('location: thankyou.html');
